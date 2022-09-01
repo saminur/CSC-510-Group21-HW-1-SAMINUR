0ce13311f48dcfba6f4a179a42570b5ab7fa74e0
saminur <saminur.islam08@gmail.com>
2022-08-31 23:05:03 -0400

    Create setup.py
    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='CSC-510-Group21-HW-1-SAMINUR',
+    version='v1.0.0',
+    description='First HW for CSC 510',
+    authors='Saminur Islam,',
+    author_email='sislam@ncsu.edu',
+    url='https://github.com/saminur/CSC-510-Group21-HW-1-SAMINUR/',
+    packages=[''],
+    long_description="""\
+        First HW for CSC 510
+    """,
+    classifiers=[
+        "License :: OSI Approved ::  MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Homework",
+    ],
+    keywords='homework',
+    license='Apache License',
+    install_requires=[]
+)

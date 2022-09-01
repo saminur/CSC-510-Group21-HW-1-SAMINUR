eb44bdb9870c3d4076d606454e698dc9104ee545
saminur <saminur.islam08@gmail.com>
2022-09-01 00:09:39 -0400

    Create __init__.py
    create the init py file

---
+++ code/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 12:08:15 2022
+
+@author: Saminur
+"""
+
+# content of __init__.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
